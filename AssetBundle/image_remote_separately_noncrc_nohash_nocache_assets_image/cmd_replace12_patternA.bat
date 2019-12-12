2f13e363ee1c5f12bd132cc4b286d03fc5b9cf8d
Juicefish <spring35857@yahoo.com.tw>
2019-12-12 14:49:02 +0800

    no message

--- AssetBundle/image_remote_separately_noncrc_nohash_nocache_assets_image/cmd_replace12_patternA.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy largeimage_remote_1_A.bundle largeimage_remote_1.bundle /Y
-copy largeimage_remote_2_A.bundle largeimage_remote_2.bundle /Y
--- AssetBundle/image_remote_separately_noncrc_nohash_nocache_assets_image/cmd_replace12_patternB.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy largeimage_remote_1_B.bundle largeimage_remote_1.bundle /Y
-copy largeimage_remote_2_B.bundle largeimage_remote_2.bundle /Y
